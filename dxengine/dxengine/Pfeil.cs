3b37854904265bcca2d1c35bc73b6ca4ed2b6cc3
Torbilicious <Torben@Binder-net.de>
2012-02-27 17:27:12 +0100

    dxengine verbessert und Fehlermeldungen eingefügt

---
+++ dxengine/dxengine/Pfeil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace dxengine
+{
+    class Pfeil
+    {
+        private Vector3 _startPunkt;
+        public Vector3 startPunkt
+        {
+            get { return _startPunkt; }
+            set { _startPunkt = value; }
+        }
+
+        private Vector3 _endPunkt;
+        public Vector3 endPunkt
+        {
+            get { return _endPunkt; }
+            set { _endPunkt = value; }
+        }
+    }
+}
