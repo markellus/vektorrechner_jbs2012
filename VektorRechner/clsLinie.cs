5539701e1e39e128a3d96e93922ea308900e8c20
markellus <marcel.bulla@gmx-topmail.de>
2012-03-03 13:55:41 +0100

    clsLinie geschrieben

--- VektorRechner/clsLinie.cs
+++ VektorRechner/clsLinie.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace VektorRechner
 {
     class clsLinie
     {
+        public clsLinie(int x1, int y1, int x2, int y2)
+        {
+            init(x1, y1, x2, y2, Pens.Black);
+        }
+
+        public clsLinie(int x1, int y1, int x2, int y2, Pen farbe)
+        {
+            init(x1, y1, x2, y2, farbe);
+        }
+
+        public void init(int x1, int y1, int x2, int y2, Pen farbe)
+        {
+        }
+
+        public int x1 { get; set; }
+        public int y1 { get; set; }
+        public int x2 { get; set; }
+        public int y2 { get; set; }
+        public Pen farbe { get; set; }
+
+        public void zeichne(Graphics graphics)
+        {
+            graphics.DrawLine(farbe, x1, y1, x2, y2);
+        }
+
     }
 }
