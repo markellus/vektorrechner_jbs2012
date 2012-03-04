9b648796be63661cbe5b077cc3ef81673f81e444
markellus <marcel.bulla@gmx-topmail.de>
2012-03-04 13:33:52 +0100

    Linienklassen erweitert

--- VektorRechner/clsLinie.cs
+++ VektorRechner/clsLinie.cs
@@ -17,9 +17,13 @@ namespace VektorRechner
         {
             init(x1, y1, x2, y2, farbe);
         }
-
         public void init(int x1, int y1, int x2, int y2, Pen farbe)
         {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.farbe = farbe;
         }
 
         public int x1 { get; set; }
--- VektorRechner/clsLinieBeschriftet.cs
+++ VektorRechner/clsLinieBeschriftet.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace VektorRechner
 {
     class clsLinieBeschriftet
     {
+        public void init(int x1, int y1, int x2, int y2, Pen farbe, string beschriftung)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.farbe = farbe;
+            this.beschriftung = beschriftung;
+        }
+
+        public int x1 { get; set; }
+        public int y1 { get; set; }
+        public int x2 { get; set; }
+        public int y2 { get; set; }
+        public Pen farbe { get; set; }
+        public string beschriftung { get; set; }
+
+        public void zeichne(Graphics graphics)
+        {
+            graphics.DrawLine(farbe, x1, y1, x2, y2);
+        }
     }
 }
