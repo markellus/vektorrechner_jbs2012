d5f87ad9362858f5e056501f1a073354aa92e20b
markellus <marcel.bulla@gmx-topmail.de>
2012-04-12 11:46:14 +0200

    Updater eingefügt

---
+++ Updater/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtStatus = new System.Windows.Forms.RichTextBox();
+            this.bwDownload = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(260, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(12, 41);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(260, 96);
+            this.txtStatus.TabIndex = 2;
+            this.txtStatus.Text = "";
+            // 
+            // bwDownload
+            // 
+            this.bwDownload.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bwDownload_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 149);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox txtStatus;
+        private System.ComponentModel.BackgroundWorker bwDownload;
+    }
+}
+
---
+++ Updater/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Ionic.Zip;
+
+namespace Updater
+{
+    public partial class Form1 : Form
+    {
+        bool Updatefertig = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bwDownload.RunWorkerAsync();
+        }
+
+        private void bwDownload_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DelegateStatusTextAktualisieren textAktualisieren = new DelegateStatusTextAktualisieren(AktualisiereStatus);
+
+            string curDir = Directory.GetCurrentDirectory();
+            txtStatus.Invoke(textAktualisieren, "Prüfe auf Updates...");
+            System.Net.WebClient client = new System.Net.WebClient();
+            byte[] tmpB = client.DownloadData("http://127.0.0.1/status.txt");//Url einfügen
+            txtStatus.Invoke(textAktualisieren, "Download wird gestartet");
+            string downloadlink = System.Text.Encoding.ASCII.GetString(tmpB);
+            downloadFile(downloadlink);
+            while (!Updatefertig) { }
+            ZipFile zip = ZipFile.Read(curDir + "/UpdatePacket.zip");
+            File.Move(curDir + "/Updater.exe", curDir + "/Updater.old.exe");
+            zip.ExtractAll(curDir);
+            txtStatus.Invoke(textAktualisieren, "Update abgeschlossen.");
+        }
+
+        private void downloadFile(string addresse)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadDataCompleted += new DownloadDataCompletedEventHandler(Completed);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+            webClient.DownloadDataAsync(new Uri(addresse));
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            DelegateProgressbarAktualisieren myProgress = new DelegateProgressbarAktualisieren(AktualisiereProgressbar);           
+            progressBar1.Invoke(myProgress, e.ProgressPercentage);
+        }
+
+        private void Completed(object sender, DownloadDataCompletedEventArgs e)
+        {
+            DelegateStatusTextAktualisieren textAktualisieren = new DelegateStatusTextAktualisieren(AktualisiereStatus);
+            txtStatus.Invoke(textAktualisieren, "Download abgeschlossen");
+            File.WriteAllBytes(Directory.GetCurrentDirectory() + "/UpdatePacket.zip", e.Result);
+            txtStatus.Invoke(textAktualisieren, "Das Update wurde als \"UpdatePacket.zip\" gespeichert.");
+            Updatefertig = true;
+        }
+
+        private delegate void DelegateStatusTextAktualisieren(string value);
+        private void AktualisiereStatus(string value)
+        {
+            txtStatus.Text += value + "\r\n";
+        }
+
+        private delegate void DelegateProgressbarAktualisieren(int value);
+        private void AktualisiereProgressbar(int value)
+        {
+            progressBar1.Value = value;
+        }
+
+        private delegate void Delegatefertig();
+        private void fertig()
+        {
+            Updatefertig = true;
+        }
+    }
+}
