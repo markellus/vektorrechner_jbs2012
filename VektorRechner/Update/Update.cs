9902bf08392d46d67b3859572c275d6aa475564b
markellus <marcel.bulla@gmx-topmail.de>
2012-04-12 11:36:15 +0200

    Update Checker eingebaut

--- VektorRechner/Update/Update.cs
+++ VektorRechner/Update/Update.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Windows.Forms;
 
 namespace VektorRechner.Update
 {
-    class Update
-    {
+    public static class Update
+    {   //prüft auf Updates und fragt, ob diese installiert weren sollen.
+        public static void AufUpdatesPrüfen(int version)
+        {
+            WebClient client = new WebClient();
+            try
+            {
+                byte[] neueVersion = client.DownloadData("");//TODO: Link einfügen
+                int neueVersionKonvertiert = Convert.ToInt32(Encoding.ASCII.GetString(neueVersion));
+
+                if (neueVersionKonvertiert > version)
+                {
+                    if (MessageBox.Show("Es ist eine neue Version verfügbar. Downloaden?", "Update", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        System.Diagnostics.Process.Start("Updater.exe");
+                        Application.Exit();
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                //sinnvoll?
+                MessageBox.Show("Überprüfung auf Updates fehlgeschlagen");
+            }
+        }
     }
 }
