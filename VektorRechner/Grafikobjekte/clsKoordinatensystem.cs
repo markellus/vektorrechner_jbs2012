67d57f048281f91912c7946ea70e804894cc5c6a
markellus <marcel.bulla@gmx-topmail.de>
2012-04-12 11:42:36 +0200

    Koordinatensystem überarbeitet
    Funktionen sind jetzt ausgelagert

--- VektorRechner/Grafikobjekte/clsKoordinatensystem.cs
+++ VektorRechner/Grafikobjekte/clsKoordinatensystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using VektorRechner.Berechnungen;
 
 namespace VektorRechner
 {
@@ -20,21 +21,12 @@ namespace VektorRechner
 
         public clsKoordinatensystem(int Skalierung, int v1x1, int v1x2, int v2x1, int v2x2, int v3x1, int v3x2, int xStart, int yStart, int rechenart)
         {
+            //Skalierung einstellen
             skalierung = Skalierung;
-
-            linieX = new clsLinie(5, 305, 605, 305);
-            linieY = new clsLinie(305, 5, 305, 605);
-
-            for (int i = 0; i < 30; i++)
-            {
-                linienBeschriftungXY.Add(new clsLinie(i * 20 + 5, 300, i * 20 + 5, 310));
-            }
-
-            for (int i = 0; i < 30; i++)
-            {
-                linienBeschriftungXY.Add(new clsLinie(300, i * 20 + 5, 310, i * 20 + 5));
-            }
-
+            KoordinatenMatrix.skalierung = skalierung;
+            ErstelleAchsen();
+            ErstelleAchsenLinien();
+            //Die Beschriftungen deklarieren
             for (int i = 0; i < 30; i++)
             {
                 if ((i - 15) != 0 && (i - 15) != -1)
@@ -53,25 +45,53 @@ namespace VektorRechner
                 }
                 beschriftung -= 1;
             }
-
+            //Die Vektoren deklarieren
+            int StartPunktX = KoordinatenMatrix.berechnePixelX(xStart);
+            int StartPunktY = KoordinatenMatrix.berechnePixelX(yStart);
             if (rechenart == 0)
             {
-                vektoren.Add(new clsVektor(v1x1 * 20 / skalierung, v1x2 * 20 / skalierung, 305 + (xStart * 20 / skalierung), 305 - (yStart * 20 / skalierung), Pens.Red));
-                vektoren.Add(new clsVektor(v2x1 * 20 / skalierung, v2x2 * 20 / skalierung, 305 + (v1x1 * 20 / skalierung), 305 - (v1x2 * 20 / skalierung), Pens.Blue));
-                vektoren.Add(new clsVektor(v3x1 * 20 / skalierung, v3x2 * 20 / skalierung, 305 + (xStart * 20 / skalierung), 305 - (yStart * 20 / skalierung), Pens.YellowGreen));
+                vektoren.Add(new clsVektor(KoordinatenMatrix.berechneStrecke(v1x1), KoordinatenMatrix.berechneStrecke(v1x2), StartPunktX, StartPunktY, Pens.Red));
+                vektoren.Add(new clsVektor(KoordinatenMatrix.berechneStrecke(v2x1), KoordinatenMatrix.berechneStrecke(v2x2), KoordinatenMatrix.berechnePixelX(v1x1), KoordinatenMatrix.berechnePixelY(v1x2), Pens.Blue));
+                vektoren.Add(new clsVektor(KoordinatenMatrix.berechneStrecke(v3x1), KoordinatenMatrix.berechneStrecke(v3x2), StartPunktX, StartPunktY, Pens.YellowGreen));
             }
 
+            else if (rechenart == 1)
+            {
+                vektoren.Add(new clsVektor(KoordinatenMatrix.berechneStrecke(v1x1), KoordinatenMatrix.berechneStrecke(v1x2), StartPunktX, StartPunktY, Pens.Red));
+                vektoren.Add(new clsVektor(KoordinatenMatrix.berechneStrecke(v2x1), KoordinatenMatrix.berechneStrecke(v2x2), StartPunktX, StartPunktY, Pens.Blue));
+                vektoren.Add(new clsVektor(KoordinatenMatrix.berechneStrecke(v3x1), KoordinatenMatrix.berechneStrecke(v3x2), StartPunktX, StartPunktY, Pens.YellowGreen));
+            }
             else
             {
-                vektoren.Add(new clsVektor(v1x1 * 20 / skalierung, v1x2 * 20 / skalierung, 305 + (xStart * 20 / skalierung), 305 - (yStart * 20 / skalierung), Pens.Red));
-                vektoren.Add(new clsVektor(v2x1 * 20 / skalierung, v2x2 * 20 / skalierung, 305 + (xStart * 20 / skalierung), 305 - (yStart * 20 / skalierung), Pens.Blue));
-                vektoren.Add(new clsVektor(v3x1 * 20 / skalierung, v3x2 * 20 / skalierung, 305 + (xStart * 20 / skalierung), 305 - (yStart * 20 / skalierung), Pens.YellowGreen));
+                vektoren.Add(new clsVektor(KoordinatenMatrix.berechneStrecke(v3x1), KoordinatenMatrix.berechneStrecke(v3x2), StartPunktX, StartPunktY, Pens.YellowGreen));
+                vektoren.Add(new clsVektor(KoordinatenMatrix.berechneStrecke(v1x1), KoordinatenMatrix.berechneStrecke(v1x2), StartPunktX, StartPunktY, Pens.Red));
             }
 
         }
 
-        public int skalierung { get; set; }
+        private void ErstelleAchsen()
+        {
+            //Die X- und Y- Linien deklarieren
+            linieX = new clsLinie(5, 305, 605, 305);
+            linieY = new clsLinie(305, 5, 305, 605);
+        }
 
+        private void ErstelleAchsenLinien()
+        {
+            //Die Querlinien deklarieren
+            for (int i = 0; i < 30; i++)
+            {
+                linienBeschriftungXY.Add(new clsLinie(i * 20 + 5, 300, i * 20 + 5, 310));
+            }
+
+            for (int i = 0; i < 30; i++)
+            {
+                linienBeschriftungXY.Add(new clsLinie(300, i * 20 + 5, 310, i * 20 + 5));
+            }
+        }
+
+        public int skalierung { get; set; }
+        //Zeichnet alle Linien, label und Vektoren
         public void zeichne(Graphics graphics)
         {
             linieX.zeichne(graphics);
