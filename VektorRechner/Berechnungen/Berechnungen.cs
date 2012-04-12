2c27b3b0ed70af7ad68cac6fcc5c9e4eb81e9b96
markellus <marcel.bulla@gmx-topmail.de>
2012-04-12 11:40:19 +0200

    Berechnungen eingefügt
    Koordinatenmatrix gebaut

--- VektorRechner/Berechnungen/Berechnungen.cs
+++ VektorRechner/Berechnungen/Berechnungen.cs
@@ -2,10 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace VektorRechner.Berechnungen
 {
-    class Berechnungen
-    {
+    public static class Berechnungen
+    {   //2 Vektoren mit beliebig vielen Dimensionen werden addiert.
+        public static double[] AddiereVektoren(double[] vektor1, double[] vektor2)
+        {
+            if (vektor1.Length != vektor2.Length)
+            {
+                MessageBox.Show("Beide Vektoren müssen dieselbe Anzahl an Dimensionen haben!");
+                return null;
+            }
+
+            double[] vektor3 = new double[vektor1.Length];
+            //Jede Dimension einzeln addieren
+            for (int i = 0; i < vektor1.Length; i++)
+            {
+                vektor3[i] = vektor1[i] + vektor2[i];
+            }
+
+            return vektor3;
+        }
+        //2 Vektoren mit beliebig vielen Dimensionen werden subtrahiert.
+        public static double[] SubtrahiereVektoren(double[] vektor1, double[] vektor2)
+        {
+            if (vektor1.Length != vektor2.Length)
+            {
+                MessageBox.Show("Beide Vektoren müssen dieselbe Anzahl an Dimensionen haben!");
+                return null;
+            }
+
+            double[] vektor3 = new double[vektor1.Length];
+
+            for (int i = 0; i < vektor1.Length; i++)
+            {
+                vektor3[i] = vektor1[i] - vektor2[i];
+            }
+
+            return vektor3;
+        }
+        //1 Vektor wird mit einer natürlichen Zahl multipliziert.
+        public static double[] MultipliziereVektoren(double[] vektor1, double[] vektor2, double multiplikator)
+        {
+            double[] vektor3 = new double[vektor1.Length];
+
+            for (int i = 0; i < vektor1.Length; i++)
+            {
+                vektor3[i] = vektor1[i] * multiplikator;
+            }
+
+            return vektor3;
+        }
     }
 }
--- VektorRechner/Berechnungen/KoordinatenMatrix.cs
+++ VektorRechner/Berechnungen/KoordinatenMatrix.cs
@@ -5,7 +5,28 @@ using System.Text;
 
 namespace VektorRechner.Berechnungen
 {
-    class KoordinatenMatrix
+    public static class KoordinatenMatrix
     {
+        public static int skalierung = 1;
+
+        public static int berechnePixelX(int value)
+        {
+            return (value * 20 / skalierung + 305);
+        }
+
+        public static int berechnePixelY(int value)
+        {
+            return (305 - value * 20 / skalierung);
+        }
+
+        public static int berechneStrecke(int value)
+        {
+            return (value * 20 / skalierung);
+        }
+
+        public static int berechneBeschriftung(int value)
+        {
+            return value * skalierung;
+        }
     }
 }
